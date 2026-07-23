f27cb3362512b70d9cefbe26d14abb196f4f21ea
Windstreak <jonnyboyx2006@gmail.com>
2026-07-23 10:30:23 -0700

    Added 11_Entry to Greenroom, readjusted layout It's done frfr this time (3rd times the charm)

--- js/greenroom.js
+++ js/greenroom.js
@@ -11,21 +11,23 @@ $(placeOverlay());
 
 $(async function placeEntries() {
     let number;
-    for (let i = 1; i <= 8; i++){
+    for (let i = 1; i <= 11; i++){
         number = i < 10? '0'+i : i;
-        if (i!=3)
+        if (i==3)
         {
+            $(".entries").append('<a href="https://drive.google.com/drive/folders/1X7hYFt84bsaQ0JVBv1zIBMcxVbYh64pA" target="blank"><img class="entry art tiltable external" id="entry_'+number+'" src='+iconFilePath+number+'_entry_icon.png loading="lazy"></a>');
+        }
+        else if(i==9){
+            $(".entries").append('<a href="https://drive.google.com/drive/u/2/folders/184XtAMQ0QdOZk92hvh6S1drWGZPP_CvM" target="blank"><img class="entry art tiltable external" id="entry_'+number+'" src='+iconFilePath+number+'_entry_icon.png loading="lazy"></a>');
+        }
+        else if(i==10){
+            $(".entries").append('<a href="https://drive.google.com/drive/folders/1vtSQq5OMe6Li6lcxsVkk0GK5HVGfnJei" target="blank"><img class="entry art tiltable external" id="entry_'+number+'" src='+iconFilePath+number+'_entry_icon.png loading="lazy"></a>');
+        }
+        else{
             $(".entries").append('<img class="entry art tiltable" id="entry_'+number+'" src='+iconFilePath+number+'_entry_icon.png loading="lazy">');
             let entry = document.querySelector('#entry_'+number);
             entry.addEventListener("click", function (e){activateOverlay(e, entryData, entryFilePath)});
         }
-        else{
-            $(".entries").append('<a href="https://drive.google.com/drive/folders/1X7hYFt84bsaQ0JVBv1zIBMcxVbYh64pA" target="blank"><img class="entry art tiltable external" id="entry_'+number+'" src='+iconFilePath+number+'_entry_icon.png loading="lazy"></a>');
-        }
     }
-    number="09";
-    $(".entries").append('<a href="https://drive.google.com/drive/u/2/folders/184XtAMQ0QdOZk92hvh6S1drWGZPP_CvM" target="blank"><img class="entry art tiltable external" id="entry_'+number+'" src='+iconFilePath+number+'_entry_icon.png loading="lazy"></a>');
-    number="10";
-    $(".entries").append('<a href="https://drive.google.com/drive/folders/1vtSQq5OMe6Li6lcxsVkk0GK5HVGfnJei" target="blank"><img class="entry art tiltable external" id="entry_'+number+'" src='+iconFilePath+number+'_entry_icon.png loading="lazy"></a>');
     scaleEntries();
 });
